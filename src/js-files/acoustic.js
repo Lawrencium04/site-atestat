b2c35f9466ddfb2a82e14f738d95d6e80613d1f1
aurentziu <laurentiulaurentiu04@gmail.com>
2023-03-31 05:27:00 -1000

    updated the construction page

--- src/js-files/acoustic.js
+++ src/js-files/acoustic.js
@@ -5,32 +5,32 @@ import '../css-files/acoustic.css'
 
 export default function Acoustic(props) {
 	let [selected, setSelectet] = useState(0);
-	let [name, setName] = useState("acoustic");
 
 	function selectPart(nr) {
-		console.log(nr + " " + selected)
 		if (nr === selected) {
-			setName('acoustic')
 			setSelectet(0);
 			return;
 		}
 		setSelectet(nr);
 
-		setName('acoustic selected');
 	}
 
   return (
-    <div className={!props.transition ? name : 'acoustic off'}>
+    <div className={!props.transition ? 'acoustic' : 'acoustic off'}>
 		<div className="title">
 			<b>acustică.</b>
 		</div>
 		<div className="guitar">
 			<img className="pic" src={guitar} alt="" />
-			<span className="body-select" onClick={() => selectPart(1)}></span>
-			<span className="neck-select" onClick={() => console.log("neck")}></span>
-			<span className="headstock-select"></span>
+			<span className={selected === 1 ? "body-select selected" : "body-select"} onClick={() => selectPart(1)}></span>
+			<span className={selected === 2 ? "neck-select selected" : "neck-select"} onClick={() => selectPart(2)}></span>
+			<span className={selected === 3 ? "headstock-select selected" : "headstock-select"} onClick={() => selectPart(3)} ></span>
 		</div>
 		<div className="body-desc">
+			{selected === 0 && <p>selectează o <b>componentă</b> pentru a afișa <b>informații.</b></p>}
+			{selected === 1 && <p><b>Corpul chitarei</b> este o cutie rezonantă formată din <b>blatură</b> (partea superioară), <b>spate</b> și <b>laterale</b>, realizată în general din lemn masiv. Blatura conține punțile și găurile de sunet, iar spatele reflectă și amplifică sunetul. <b>Forma</b> și <b>dimensiunile</b> corpului pot influența sunetul și există mai multe variante disponibile, precum dreadnought, jumbo, concert, auditorium sau parlor.</p>}
+			{selected === 2 && <p><b>Gâtul chitarei</b> este partea lungă și subțire care susține tastatura și se extinde de la corpul chitarei. Este de obicei construit din lemn dur și poate avea o formă plată sau rotunjită. <b>Tastatura</b> este o placă de lemn așezată pe partea superioară a gâtului, care conține fante pentru fiecare dintre corzi și puncte de referință pentru chitariști. Lungimea și forma gâtului chitarei variază în funcție de modelul instrumentului.</p>}
+			{selected === 3 && <p><b>Headstock-ul</b> chitarei este partea de la capătul gâtului care conține mecanismele de acordaj ale corzilor și este decorat cu ornamente. Este important nu doar pentru aspectul estetic al chitarei, ci și pentru funcționalitatea sa, permițând chitaristului să <b>ajusteze tensiunea corzilor</b>.</p>}
 		</div>
 	</div>
   )
