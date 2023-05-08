ac2f53c9a927122c47abb5888af9ecde28e1df20
Aurentziu <69503769+Lawrencium04@users.noreply.github.com>
2023-05-08 10:01:19 +0000

    added top section

--- src/js-files/App.js
+++ src/js-files/App.js
@@ -19,6 +19,7 @@ export default function App() {
 								<Navbar/>
 								<Home/>
 								<Construction/>
+								<Tops/>
 							</>
 						}
 					/>
--- src/js-files/tops.js
+++ src/js-files/tops.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "../css-files/tops.css"
+import guitar from '../assets/png/Guitar.png'
 
 export default function Tops(props) {
     return (
         <>
-            <div id={!props.transition ? 'tops' : 'tops off'}>
-
+            <div id="top">
+                <div className="beginner">
+                    <h1>EPIPHONE DR-100</h1>
+                    <img src={guitar}/>
+                    <p>The most beginner friendly guitar for it's good quality build and low price.</p>
+                </div>
+                <div className="friends">
+                    <h1>TITLE2</h1>
+                    <img src={guitar}/>
+                    <p>The guitar you need when you go out with your friends.</p>
+                </div>
+                <div className="overall">
+                    <h1>TITLE3</h1>
+                    <img src={guitar}/>
+                    <p>The best guitar overall.</p>
+                </div>
             </div>
         </>
     )
